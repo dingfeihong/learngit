5c54f37fb4014ba055eaafe319ad28c867a905bf
cisl <cisl@cisldeiMac.local>
2017-11-09 16:24:41 +0800

    11.9  编译完成

--- MyAlamofire/UserModel.swift
+++ MyAlamofire/UserModel.swift
@@ -7,42 +7,35 @@
 //
 
 import Foundation
-class UserModel{
-    private var m_name: String?
+class UserModel: NSObject, NSCoding{
     private var m_id: String?
     private var m_pswd: String?
-    private var m_status = false
-    private var m_time: String?
-    init(){
-        
-    }
-    func load(name: String,pswd: String,id: String,status :Bool, time: String) {
-        m_name = name
-        m_pswd = pswd
-        m_id = id
-        m_status = status
-        m_time = time
-    }
     
-    func GetStatus() -> Bool?{
-        return m_status
-    }
-    func SetStatus(status: Bool){
-        self.m_status = status
-    }
     func GetID() -> String{
         return m_id!
     }
-    func GetName() -> String{
-        return m_name!
+    
+    func GetPsd() -> String{
+        return m_pswd!
     }
     
-    func GetTime() -> String{
-        return m_time!
+    
+    required init(id:String="", pswd:String="") {
+        self.m_id = id
+        self.m_pswd = pswd
     }
-    func SetTime(time: Date){
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        self.m_time = formatter.string(from: time)
+    
+    //从object解析回来
+    required init(coder decoder: NSCoder) {
+        self.m_id = decoder.decodeObject(forKey: "id") as? String ?? ""
+        self.m_pswd = decoder.decodeObject(forKey: "password") as? String ?? ""
     }
+    
+    //编码成object
+    func encode(with coder: NSCoder) {
+        coder.encode(m_id, forKey:"id")
+        coder.encode(m_pswd, forKey:"password")
+    }
+    
 }
+
